a06f760a013de2ebcf24873abde4c399f25fc8ad
Arthur Rodrigues Proença <arthur.rodriguesproenca@gmail.com>
2023-05-21 18:53:41 -0300

    add: primeiras configurações do data access layer

---
+++ DAL - OCN/src/main/java/dev/friday/com/dal/DalOcnApplication.java
@@ -0,0 +1,13 @@
+package dev.friday.com.dal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalOcnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalOcnApplication.class, args);
+	}
+
+}
---
+++ DAL - OCN/src/test/java/dev/friday/com/dal/DalOcnApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.friday.com.dal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalOcnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
